3edab26bb1eb9ad6541cc23447de18e11ada6273
rainayangg <raina.yang.chn@gmail.com>
2025-11-12 15:29:00 +0100

    Add lab4

---
+++ Lab 4/SDN/install_ryu.sh
@@ -0,0 +1,5 @@
+apt-get update
+apt install gcc python3-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev zlib1g-dev
+pip3 install --upgrade pip
+pip3 install --upgrade .
+pip3 install -r requirements.txt
