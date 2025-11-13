9037d5d8ccfafb4aeded956581f8d440182121da
Mahdi Hosseini <shossein@tsf-452-wpa-4-250.epfl.ch>
2025-11-13 18:21:10 +0100

    Updating SDN files

--- Lab 4/SDN/install_ryu.sh
+++
@@ -1,5 +0,0 @@
-apt-get update
-apt install gcc python3-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev zlib1g-dev
-pip3 install --upgrade pip
-pip3 install --upgrade .
-pip3 install -r requirements.txt
